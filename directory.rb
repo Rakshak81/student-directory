3d5bd48882248a387e4e7f9ee440c5cf92e740de
Raks <rakshak_81@hotmail.com>
2022-01-19 07:59:09 +0000

    Step 2

--- directory.rb
+++ directory.rb
@@ -11,3 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# we print the number of students
+print "Overall, we have "
+# we used print as it is important not to add new line
+print 11
+puts "great students"
